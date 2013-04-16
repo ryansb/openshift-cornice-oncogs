236cb3a5ed2f31ce7f1486ae2f176491d5b970a1
Ryan Brown <ryansb@csh.rit.edu>
2013-04-16 11:23:32 -0400

    Create tables automatically

--- wsgi/corniceapp/corniceapp/models/__init__.py
+++ wsgi/corniceapp/corniceapp/models/__init__.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.exc import ProgrammingError
 from zope.sqlalchemy import ZopeTransactionExtension
 
 Base = declarative_base()
@@ -13,5 +14,17 @@ DBSession = scoped_session(
     sessionmaker(bind=_engine, extension=ZopeTransactionExtension())
 )
 
+
 from corniceapp.models.users import User
-# Import your models here
+
+# If the database isn't initialized, initialize it!
+try:
+    User.all()
+except ProgrammingError, e:
+    import transaction
+
+    metadata = Base.metadata
+    metadata.create_all(_engine)
+
+    u = User('ryansb', 'somepass', 'Ryan Brown', 'ryansb@csh.rit.edu')
+    u.put()
